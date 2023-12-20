9d4e8ad6e83d93af813563c877ef90bf2809efe9
ashish kha <ashish.kha@grab.in>
2023-12-20 22:38:21 +0530

    new changes done14

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def recommend_movies():
 if __name__ == '__main__':
  name = new2.get_movie_name()
  print(f"The movie name is: {name}")
-app.run(host='0.0.0.0',debug=True,port=3000)
+app.run(debug=True)
