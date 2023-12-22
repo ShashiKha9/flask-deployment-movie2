bc1ed28e58d6d9633d7590364a49c58bf5a42a2d
ashish kha <ashish.kha@grab.in>
2023-12-23 00:17:07 +0530

    new changes done40

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ import new2
 
 app = Flask(__name__)
 # CORS(app)  # Enable CORS for all routes done
-@app.route('/movie', methods=['GET'])
+
+@app.route('/')
+def index():
+    return  'result'
 def recommend_movies():
 
     res = new2.results(request.args.get('title'))
@@ -27,4 +30,4 @@ def recommend_movies():
 if __name__ == '__main__':
  name = new2.get_movie_name()
  print(f"The movie name is: {name}")
-app.run(host='0.0.0.0',debug=True,port=10000,use_reloader=False)
+app.run(debug=True,port=10000,use_reloader=False)
