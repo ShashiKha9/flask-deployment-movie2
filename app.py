c4b4efb16577d6cdab0dd99fdabee77fa5516050
ashish kha <ashish.kha@grab.in>
2023-12-28 11:36:53 +0530

    new changes done64

--- app.py
+++ app.py
@@ -14,24 +14,24 @@ from flask_cors import CORS
 
 import new2
 
-load_dotenv()
-secret_key = os.getenv("SECRET_KEY")
-debug_mode = os.getenv("DEBUG")
+# load_dotenv()
+# secret_key = os.getenv("SECRET_KEY")
+# debug_mode = os.getenv("DEBUG")
 
 app = Flask(__name__)
 CORS(app)  # /Enable CORS for all routes done
-
-app.config['SECRET_KEY'] = secret_key
-if debug_mode is not None:
-    app.config['DEBUG'] = debug_mode.lower() == 'true'
-else:
-    # Handle the case when debug_mode is None (e.g., set a default value)
-    app.config['DEBUG'] = False  # Or any other default value you want
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+#
+# app.config['SECRET_KEY'] = secret_key
+# if debug_mode is not None:
+#     app.config['DEBUG'] = debug_mode.lower() == 'true'
+# else:
+#     # Handle the case when debug_mode is None (e.g., set a default value)
+#     app.config['DEBUG'] = False  # Or any other default value you want
+# app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 @app.route('/')
 def index():
-        secret_key = app.config['SECRET_KEY']
+        # secret_key = app.config['SECRET_KEY']
 
         return 'Hello'
 
@@ -58,4 +58,4 @@ def recommend_movies(title):
 if __name__ == '__main__':
  name = new2.get_movie_name()
  print(f"The movie name is: {name}")
-app.run(host='0.0.0.0',debug=True,port=10000,use_reloader=False,ssl_context=("cert.pem", "key.pem"))
+app.run(host='0.0.0.0',debug=True,port=8000,use_reloader=False)
